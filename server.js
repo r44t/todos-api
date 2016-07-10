1b35796b9ed2a7e4f80f092b414bc9a17352e1d6
Ghazi <ghazi@ghazi.im>
2016-07-10 11:58:24 +0300

    Finial static todos

--- server.js
+++ server.js
@@ -23,9 +23,16 @@ app.get('/todos', function (req, res) {
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, {completed: false});
   }
-  // if has property && completed === 'true'
+
+    // if has property && completed === 'true'
   //    filteredTodos = _.where(filteredTodos, ?)
   // else if has prop && completed if 'false'
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
+
 
 	res.json(filteredTodos);
 });
