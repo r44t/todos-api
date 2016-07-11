f82cbe8a803dbd358523626db54f599b52522cb3
Ghazi <ghazi@ghazi.im>
2016-07-11 11:02:09 +0300

    Add Sequelize demo

---
+++ playground/variable-playground.js
@@ -0,0 +1,26 @@
+//var person = {
+  //name: 'ghazi',
+  //age: 21
+//};
+
+
+//function updatePerson(obj) {
+  ////obj = {
+  ////  name: 'ghazi',
+  ////  age: 24
+  ////};
+  //obj.age = 34
+//}
+
+//updatePerson(person);
+//console.log(person);
+
+var grades = [50, 37];
+
+function addGrade (gradesArr) {
+  gradesArr.push(55);
+  debugger;
+}
+
+addGrade(grades);
+console.log(grades);
